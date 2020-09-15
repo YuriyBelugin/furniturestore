7fc2716d940e2096247496ab971172a84f48f586
Yury Bialuhin <yurka_belugin@mail.ru>
2020-09-16 00:00:19 +0300

    export

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,43 +17,43 @@ import static com.company.reader.ReaderConstants.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException, IOException {
-//        TypeFurnitureReader typeFurnitureReader = new TypeFurnitureReader(FURNITURE_IMPORT_FILE);
-//        List<TypeFurniture>  typeFurniture = typeFurnitureReader.readTypeFurniture();
+        TypeFurnitureReader typeFurnitureReader = new TypeFurnitureReader(FURNITURE_IMPORT_FILE);
+        List<TypeFurniture>  typeFurniture = typeFurnitureReader.readTypeFurniture();
 
-//        System.out.println(typeFurniture);
+        System.out.println(typeFurniture);
 
-//        BrandReader brandReader = new BrandReader(BRAND_IMPORT_FILE);
-//        List<Brand>  brand =brandReader.readBrands();
-//
-//        System.out.println(brand);
+        BrandReader brandReader = new BrandReader(BRAND_IMPORT_FILE);
+        List<Brand>  brand =brandReader.readBrands();
 
-//        ProductReader productReader = new ProductReader(PRODUCT_IMPORT_FILE);
-//        List<Product>  products =productReader.readProducts();
-//
-//        System.out.println(products);
+        System.out.println(brand);
 
-//        for (TypeFurniture typeFurniture1 : typeFurniture){
-//            try {
-//                TypeFurnitureConnector.add(typeFurniture1);
-//            }catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        for (Brand brand1 : brand){
-//            try {
-//               BrandConnector.add(brand1);
-//            }catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        for (Product product : products){
-//            try {
-//                ProductConnector.add(product);
-//            }catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
+        ProductReader productReader = new ProductReader(PRODUCT_IMPORT_FILE);
+        List<Product>  products =productReader.readProducts();
+
+        System.out.println(products);
+
+        for (TypeFurniture typeFurniture1 : typeFurniture){
+            try {
+                TypeFurnitureConnector.add(typeFurniture1);
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (Brand brand1 : brand){
+            try {
+               BrandConnector.add(brand1);
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        for (Product product : products){
+            try {
+                ProductConnector.add(product);
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
         List<FullInfoDto> dtos = FullInfoConnector.getAll();
 
